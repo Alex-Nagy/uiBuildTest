8858a2b0e2db4048e27a45b4fc9d9a272e7207eb
Alexander Nagy <nagyal3x@gmail.com>
2021-12-12 19:57:32 +0100

    done as pic

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+const HTML = () => {
+  const root = document.getElementById("root");
+  root.insertAdjacentHTML(
+    "beforeend",
+    `
+    <h1>Jelentkezés</h1>
+    <input placeholder="Név">
+    <input placeholder="Email cím">
+    <div id="elfogad">
+    <input type="checkbox" id="cb"> 
+    <label for="cb">Elolvastam és elfogadom az <a href='#'>Adatkezelési Tájékoztatót.</a></label>
+    </div>
+    <button>Elküld</button>
+    `
+  );
+};
+
 function loadEvent() {
-    console.log('the page has loaded');
-    
+  console.log("the page has loaded");
+  HTML();
 }
 
 window.addEventListener("load", loadEvent);
